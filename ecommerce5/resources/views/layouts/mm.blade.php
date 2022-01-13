ce380da06d8b61913338c4277f66d60584b16c85
Mahdi_Ihsan <mahdiihsan16110@gmail.com>
2022-01-13 17:11:30 +0600

    This is the first commit for this project

---
+++ ecommerce5/resources/views/layouts/mm.blade.php
@@ -0,0 +1,13 @@
+
+
+
+jkn registration korci tkn ekta error msg show krsea kintu database e thik e add hye gese,,,error msg ta kmne slove korbo,,,,,,
+r  admin panel a dhuka jacce na,,,
+admin to kaj korse thik moto
+ji vai,,,,ekn ektu sign?registration ta check kre deken
+ok mail trape er user name and pass den
+ami discunnect kortese apnar user name and pass dawa hoyle message den
+
+solved anything else
+
+reaily you deserve a treat!!!thank you so much vaiya
